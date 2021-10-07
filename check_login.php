e84b66164d746076423cff972376c3076f38b056
P Y <paphana.yiwsiw@gmail.com>
2021-10-07 21:36:02 +0700

    Login/Logout Core
    Customer Login and Shop Login

---
+++ check_login.php
@@ -0,0 +1,32 @@
+<?php
+    include('conn_db.php');
+
+    $username = $_POST["username"];
+    $pwd = $_POST["pwd"];
+
+    $query = "SELECT c_username,c_firstname,c_lastname FROM customer WHERE
+    c_username = '$username' AND c_pwd = '$pwd' LIMIT 0,1";
+
+    $result = $mysqli -> query($query);
+    if($result -> num_rows == 1){
+        //customer login
+        $row = $result -> fetch_array();
+        session_start();
+        $_SESSION["username"] = $username;
+        $_SESSION["firstname"] = $row["c_firstname"];
+        $_SESSION["lastname"] = $row["c_lastname"];
+        ?>
+        <script>
+            alert("Welcome to EATERIO");
+            window.location = "index.php";
+        </script>
+        <?php
+    }else{
+        ?>
+        <script>
+            alert("Wrong username and/or password!");
+            history.back();
+        </script>
+        <?php
+    }
+?>
